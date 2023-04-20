3b12bc72ef73337a5c4b5042d0031096f9cd0ac7
FlamerNZ <brendanlaw@gmail.com>
2023-04-20 13:04:16 -0700

    First checkin, bumble, worn out plus a few Hind skins

---
+++ Liveries/Mi-8MT/Bumble/description.lua
@@ -0,0 +1,185 @@
+livery = {
+    	{"mi_8_tex1", 0 ,"mi_8_tex_1",false};
+	
+	{"mi_8_tex1", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	
+	{"mi_8_tex2", 0 ,"mi_8_tex_2",false};
+	
+	{"mi_8_tex2", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	
+	{"mi_8_tex3", 0 ,"mi_8_tex_3",false};
+	
+	{"mi_8_tex3", ROUGHNESS_METALLIC,"mi_8_tex_3_Mat_RoughMet",false};
+	
+	{"mi_8_tex4", 0 ,"mi_8_tex_4",false};
+	
+	{"mi_8_tex4", ROUGHNESS_METALLIC,"mi_8_tex_4_Mat_RoughMet",false};
+
+
+	--Weapons
+
+	{"b8v20",	DIFFUSE			,	"b8v20_bumble_diff", false};
+	{"b8v20",	NORMAL_MAP			,	"b8v20_nm", true};
+	{"b8v20",	SPECULAR			,	"b8v20_diff_bumble_roughmet", false};
+
+	{"upk_23_250",	DIFFUSE			,	"upk_23_250_bumble", false};
+	{"upk_23_250",	SPECULAR			,	"upk_23_250_bumble_spec_rgb", false};
+
+	{"GUV-7800_main",	DIFFUSE			,	"guv-7800_bumble", false};
+	{"GUV-7800_main",	SPECULAR			,	"guv-7800_bumble_spec", false};
+
+	--Door Gunner A
+
+	{"pilot_MI8_02",	DIFFUSE			,	"pilot_mi8_02", false};
+	--{"MI-8_guns_l",	DIFFUSE			,	"mi_8_gun", false};
+	--{"MI-8_guns_l",	SPECULAR		,	"mi_8_gun", false};
+	{"Kord_Mat1",	DIFFUSE			,		"kord_skin1", false};
+	--{"Kord_Mat1",	NORMAL_MAP			,	"kord_skin1_bump", false};
+	{"Kord_Mat1",	ROUGHNESS_METALLIC			,"kord_skin1_RoughMet", false};
+	--{"Kord_Mat1",	DIFFUSE			,		"kord_skin1", false};
+	--{"Kord_Mat1",	NORMAL_MAP			,	"kord_skin1_bump", false};
+	--{"Kord_Mat1",	ROUGHNESS_METALLIC			,	"kord_skin1_RoughMet", false};
+
+	--Door Gunner B
+
+	{"mi_8_tex3",	DIFFUSE			,		"mi_8_tex_3", false};
+	--{"mi_8_tex3",	NORMAL_MAP			,	"mi_8_tex_3_nm", true};
+	--{"mi_8_tex3",	SPECULAR			,	"mi_8_tex_3_spec", true};
+	--{"mi_8_tex3",	DAMAGE				,	"mi_8_damage_main_2", true};
+	{"pilot_MI8_02",	DIFFUSE			,	"pilot_mi8_02", false};
+
+		
+	
+	{"EVU", 0 , "evu_texture", false};
+	--{"EVU",	SPECULAR			,	"evu_texture_spec", false};
+	{"EVU", ROUGHNESS_METALLIC,"evu_texture_RoughMet", false};
+	
+	{"pilot_MI8_body", 0 ,"pilot_MI8",false};
+	{"pilot_MI8_patch",	DIFFUSE			,	"pilot_mi8_patch", false};
+	{"ZSH-7",	DIFFUSE			,	"zsh7", false};	--Helmet
+	
+	{"glass_mi-8",	DIFFUSE			,	"mi_8_glass", false};
+	{"mi_8_glass",	DIFFUSE			,	"mi_8_glass", false};
+
+	{"mi-8_tex_3", 0, "mi_8_tex_3", false};          -- still rotors
+	{"rotors_opacity", 0, "mi_8_rotors_opa", false};  -- moving rotors
+
+
+
+	{"mi_8_ENGINE_NUMBER_001", 0 ,"mi_8_tex_1",false};
+	{"mi_8_ENGINE_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_ENGINE_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_ENGINE_NUMBER_010", 0 ,"mi_8_tex_1",false};
+	{"mi_8_ENGINE_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_ENGINE_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_ENGINE_NUMBER_100", 0 ,"mi_8_tex_1",false};
+	{"mi_8_ENGINE_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_ENGINE_NUMBER_100", DECAL ,"empty",true};
+
+	
+	{"mi_8_SMALL_NUMBER_001", 0 ,"mi_8_tex_1",false};
+	{"mi_8_SMALL_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_SMALL_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_SMALL_NUMBER_010", 0 ,"mi_8_tex_1",false};
+	{"mi_8_SMALL_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_SMALL_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_SMALL_NUMBER_100", 0 ,"mi_8_tex_1",false};
+	{"mi_8_SMALL_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_SMALL_NUMBER_100", DECAL ,"empty",true};
+	
+	
+    
+	{"mi_8_BORT_NUMBER_001", 0 ,"mi_8_tex_1",false};
+	{"mi_8_BORT_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_BORT_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_BORT_NUMBER_010", 0 ,"mi_8_tex_1",false};
+	{"mi_8_BORT_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_BORT_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_BORT_NUMBER_100", 0 ,"mi_8_tex_1",false};
+	{"mi_8_BORT_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_1_Mat_RoughMet",false};
+	{"mi_8_BORT_NUMBER_100", DECAL ,"empty",true};        
+
+
+	
+	{"mi_8_BALKA_2_1_NUMBER_001", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_2_1_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_2_1_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_2_1_NUMBER_010", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_2_1_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};   
+	{"mi_8_BALKA_2_1_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_2_1_NUMBER_100", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_2_1_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};   
+	{"mi_8_BALKA_2_1_NUMBER_100", DECAL ,"empty",true};
+	
+	
+	
+	
+	
+	
+	{"mi_8_BALKA_1_1_NUMBER_001", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_1_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_1_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_1_NUMBER_010", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_1_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_1_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_1_NUMBER_100", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_1_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_1_NUMBER_100", DECAL ,"empty",true};
+
+	
+	
+	{"mi_8_BALKA_1_2_NUMBER_001", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_2_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_2_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_2_NUMBER_010", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_2_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_2_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_2_NUMBER_100", 0 ,"mi_8_tex_2",false};  
+	{"mi_8_BALKA_1_2_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};    
+	{"mi_8_BALKA_1_2_NUMBER_100", DECAL ,"empty",true};
+	
+	
+	
+	{"mi_8_BALKA_1_3_NUMBER_001", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_3_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};   
+	{"mi_8_BALKA_1_3_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_3_NUMBER_010", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_3_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false}; 
+	{"mi_8_BALKA_1_3_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_BALKA_1_3_NUMBER_100", 0 ,"mi_8_tex_2",false};
+	{"mi_8_BALKA_1_3_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_BALKA_1_3_NUMBER_100", DECAL ,"empty",true};
+
+	
+	{"mi_8_SMALL_BALKA_NUMBER_001", 0 ,"mi_8_tex_2",false};
+	{"mi_8_SMALL_BALKA_NUMBER_001", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false}; 
+	{"mi_8_SMALL_BALKA_NUMBER_001", DECAL ,"empty",true};
+
+    {"mi_8_SMALL_BALKA_NUMBER_010", 0 ,"mi_8_tex_2",false};
+	{"mi_8_SMALL_BALKA_NUMBER_010", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_SMALL_BALKA_NUMBER_010", DECAL ,"empty",true};
+
+    {"mi_8_SMALL_BALKA_NUMBER_100", 0 ,"mi_8_tex_2",false};
+	{"mi_8_SMALL_BALKA_NUMBER_100", ROUGHNESS_METALLIC,"mi_8_tex_2_Mat_RoughMet",false};
+	{"mi_8_SMALL_BALKA_NUMBER_100", DECAL ,"empty",true};
+	
+	
+
+
+}
+name = "Bumble Mil Spec"
+countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO", "ITA", "INS", "AUS","HUN","IND","IRN","IRQ","JPN","KAZ","PRK","PAK","POL","ROU","SAU","SRB","SVK","KOR","SWE","SYR","YEM","VNM","VEN","TUN","THA","SDN","PHL","MAR","MEX","MYS","LBY","JOR","IDN","HND","ETH","CHL","BRA","BHR","NZG","YUG","SUN","RSI","BLR","CHN","CZE","HRV","EGY","FIN","GRC","BGR","AUSAF","AUT","SUI"}
