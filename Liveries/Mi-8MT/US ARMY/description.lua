a9239d0ae992cdb04af63619de01e1c8070ed15c
Brendan Law <brendan.law.contractor@pepsico.com>
2023-04-21 16:17:56 -0700

    Black and Navy Mi-8

---
+++ Liveries/Mi-8MT/US ARMY/description.lua
@@ -0,0 +1,138 @@
+livery = {
+    {"mi_8_tex1", 0 ,"mi_8_tex_1",false};
+	{"mi_8_tex1", 2 ,"mi_8_tex_1_spec",false};
+	
+	{"mi_8_tex2", 0 ,"mi_8_tex_2",false};
+	{"mi_8_tex2", 2 ,"mi_8_tex_2_spec",false};
+	
+	{"mi_8_tex3", 0 ,"mi_8_tex_3",false};
+	{"mi_8_tex3", 2 ,"mi_8_tex_3_spec",false};
+	
+	{"mi_8_tex4", 0 ,"mi_8_tex_4",false};
+	{"mi_8_tex4", 2 ,"mi_8_tex_4_spec",false};
+	
+	{"pilot_MI8_body", 0 ,"pilot_MI8_PL_601_13",false};	
+	
+	{"pilot_MI8_patch", 0 ,"pilot_MI8_patch",false};	
+	
+	
+	{"EVU", 0 , "evu_texture", false};
+	{"EVU", 2 , "evu_texture_spec", false};
+
+	
+	
+	
+-- Numbers Large On Engine
+		{"mi_8_ENGINE_NUMBER_001", 0 ,"mi_8_tex_1",false};
+		{"mi_8_ENGINE_NUMBER_001", 2 ,"mi_8_tex_1_spec",false};     
+		{"mi_8_ENGINE_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_ENGINE_NUMBER_010", 0 ,"mi_8_tex_1",false};
+		{"mi_8_ENGINE_NUMBER_010", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_ENGINE_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_ENGINE_NUMBER_100", 0 ,"mi_8_tex_1",false};
+		{"mi_8_ENGINE_NUMBER_100", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_ENGINE_NUMBER_100", DECAL ,"empty",false};
+	    
+	-- Numbers Small Below Engine
+		{"mi_8_SMALL_NUMBER_001", 0 ,"mi_8_tex_1",false};
+		{"mi_8_SMALL_NUMBER_001", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_SMALL_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_SMALL_NUMBER_010", 0 ,"mi_8_tex_1",false};
+		{"mi_8_SMALL_NUMBER_010", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_SMALL_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_SMALL_NUMBER_100", 0 ,"mi_8_tex_1",false};
+		{"mi_8_SMALL_NUMBER_100", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_SMALL_NUMBER_100", DECAL ,"empty",false};
+		
+	-- Numbers Large Below Exhaust / Middle
+		{"mi_8_BORT_NUMBER_001", 0 ,"mi_8_tex_1",false};
+		{"mi_8_BORT_NUMBER_001", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_BORT_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_BORT_NUMBER_010", 0 ,"mi_8_tex_1",false};
+		{"mi_8_BORT_NUMBER_010", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_BORT_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_BORT_NUMBER_100", 0 ,"mi_8_tex_1",false};
+		{"mi_8_BORT_NUMBER_100", 2 ,"mi_8_tex_1_spec",false};
+		{"mi_8_BORT_NUMBER_100", DECAL ,"empty",false};
+		
+	-- Numbers Medium Forward Section of Tailboom
+		{"mi_8_BALKA_2_1_NUMBER_001", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_2_1_NUMBER_001", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_2_1_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_2_1_NUMBER_010", 0 ,"mi_8_tex_2",false};     
+		{"mi_8_BALKA_2_1_NUMBER_010", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_2_1_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_2_1_NUMBER_100", 0 ,"mi_8_tex_2",false};    
+		{"mi_8_BALKA_2_1_NUMBER_100", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_2_1_NUMBER_100", DECAL ,"empty",false};
+	
+	-- Numbers Medium Rightside Above Doppler Radar
+		{"mi_8_BALKA_1_1_NUMBER_001", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_1_NUMBER_001", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_1_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_1_NUMBER_010", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_1_NUMBER_010", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_1_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_1_NUMBER_100", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_1_NUMBER_100", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_1_NUMBER_100", DECAL ,"empty",false};
+	
+	-- Numbers Medium Leftside Above Doppler Radar
+		{"mi_8_BALKA_1_2_NUMBER_001", 0 ,"mi_8_tex_2",false}; 
+		{"mi_8_BALKA_1_2_NUMBER_001", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_2_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_2_NUMBER_010", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_2_NUMBER_010", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_2_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_2_NUMBER_100", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_2_NUMBER_100", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_2_NUMBER_100", DECAL ,"empty",false};
+		
+	-- Numbers Medium Leftside of Tailboom ( before Stabilizer)
+		{"mi_8_BALKA_1_3_NUMBER_001", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_3_NUMBER_001", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_3_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_3_NUMBER_010", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_3_NUMBER_010", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_3_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_BALKA_1_3_NUMBER_100", 0 ,"mi_8_tex_2",false};
+		{"mi_8_BALKA_1_3_NUMBER_100", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_BALKA_1_3_NUMBER_100", DECAL ,"empty",false};
+	
+	-- Numbers Small ( before Stabilizer)
+		{"mi_8_SMALL_BALKA_NUMBER_001", 0 ,"mi_8_tex_2",false};
+		{"mi_8_SMALL_BALKA_NUMBER_001", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_SMALL_BALKA_NUMBER_001", DECAL ,"empty",false};
+	
+	    {"mi_8_SMALL_BALKA_NUMBER_010", 0 ,"mi_8_tex_2",false};
+		{"mi_8_SMALL_BALKA_NUMBER_010", 2 ,"mi_8_tex_2_spec",false};
+		{"mi_8_SMALL_BALKA_NUMBER_010", DECAL ,"empty",false};
+	
+	    {"mi_8_SMALL_BALKA_NUMBER_100", 0 ,"mi_8_tex_2",false};
+		{"mi_8_SMALL_BALKA_NUMBER_100", 2 ,"mi_8_tex_2_spec",false};
+        {"mi_8_SMALL_BALKA_NUMBER_100", DECAL ,"empty",false};
+
+	
+}
+name = "US ARMY"
+countries = {"USA", "FRA", "SPN", "NETH", "TUR", "GER", "NOR", "CAN", "UK", "ISR", "ITA", "INS", "AUS"}
+
+custom_args = 
+{
+	[457] = 0.1, -- EVU off
+}
+
